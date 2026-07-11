9e2cc37ba5b96908a671dc80db317909b0537c43
chunglop0781 <chunglop0781@gmail.com>
2026-07-11 09:41:04 +0700

    Add files via upload

---
+++ cpp-28tech/buoiChinh/buoi2/Untitled3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+
+
+using namespace std;
+
+int main(){
+	int x, y; cin >> x >> y;
+	long long ketqua = pow(x, y);
+	cout << ketqua << endl;
+	return 0;
+}
