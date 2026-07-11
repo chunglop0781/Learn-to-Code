0bebb11317502c24d93a7f5a76df23e5802ffa2d
chunglop0781 <chunglop0781@gmail.com>
2026-07-11 09:29:53 +0700

    Add files via upload

---
+++ cpp-28tech/buoiChinh/buoi2/Untitled1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+	int x; long long y;
+	char c; float f;
+	double d;
+	cout << "Vui long nhap x, y, c, f, d:" << endl;
+	cin >> x >> y >> c >> f >> d;
+	cout << "Du lieu:" << endl << x << endl << y << endl << c << endl << f << endl << d << endl;
+	return 0;
+}
