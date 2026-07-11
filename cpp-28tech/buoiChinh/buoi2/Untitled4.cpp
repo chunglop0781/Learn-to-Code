b3df6648c834c2efcaa536ea9018d3ea31b63615
chunglop0781 <chunglop0781@gmail.com>
2026-07-11 09:44:50 +0700

    Add files via upload

---
+++ cpp-28tech/buoiChinh/buoi2/Untitled4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+
+
+using namespace std;
+
+int main(){
+	int n; cin >> n;
+	cout << fixed << setprecision(2) << sqrt(n) << endl;
+	cout << fixed << setprecision(3) << cbrt(n) << endl;
+	return 0;
+}
