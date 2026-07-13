faf6fd056a38f87b8b07998da80a84628c11b814
chunglop0781 <chunglop0781@gmail.com>
2026-07-13 15:03:16 +0700

    Add files via upload

---
+++ cpp-28tech/buoiChinh/buoi5/Untitled-1.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//nested loop
+int main() {
+
+
+    long long n; cin >> n;
+    int dem = 0;
+    if(n < 0) {
+        n *= -1;
+    }
+    else if(n == 0){
+        cout << "1" << endl;
+        return 0;
+    } else {
+        while (n != 0) {
+            dem++;
+            n /= 10;
+        }
+        cout << dem << endl;
+    }
+
+
+    // int n = 1234;
+    // int dem = 0;
+    // while (n != 0) {
+    //     dem++;
+    //     n /= 10;
+    // }
+    // cout << dem << endl;
+
+
+    // for(int i = 1; i <= 4; i++) {
+    //     for (int j = 1; j <= i; j+=2) {
+    //         cout << i << " " << j << endl;
+    //     }
+    // }
+    
+
+    // for (int i = 1; i <= 3; i++) {
+    //     cout << "Truoc" << endl;
+    //     for (int j = 1; j <= 2; j++) {
+    //         cout << i << " " << j << endl;
+    //     }
+    //     cout << "Sau" << endl;
+    // }
+
+
+    return 0;
+}
