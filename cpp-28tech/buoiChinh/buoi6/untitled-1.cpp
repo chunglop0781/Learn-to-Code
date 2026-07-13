f1cee4f870be1ea4616fa1d1a64576c1384dbb81
chunglop0781 <chunglop0781@gmail.com>
2026-07-13 22:50:23 +0700

    Create untitled-1.cpp

---
+++ cpp-28tech/buoiChinh/buoi6/untitled-1.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+
+    //Ve hinh 3
+
+
+    //Ve hinh 2
+
+    // int n; cin >> n;
+    // cout << endl;
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= i; j++) {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // for (int i = 1; i <= n; i++) {
+    //    for (int j = 1; j <= n - i + 1; j++) {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // for (int i = 1; i <= n; i++) {
+    //    for (int j = 1; j <= n; j++) {
+    //         if (j <= n-i) {
+    //             cout << " ";
+    //         }
+    //         else{
+    //             cout << "*";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // for (int i = 1; i <= n; i++) {
+    //    for (int j = 1; j <= n; j++) {
+    //         if (j >= i) {
+    //             cout << "*";
+    //         }
+    //         else{
+    //             cout << " ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // for (int i = 1; i <= n; i++) {
+    //    for (int j = 1; j <= i; j++) {
+    //         if (i == 1 || i == n || j == 1 || j == i) {
+    //             cout << "*";
+    //         }
+    //         else{
+    //             cout << " ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+
+
+    //Ve hinh 1
+
+    // int n; cin >> n;
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= n; j++) {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= n; j++) {
+    //         if (i == 1 || i == n || j == 1 || j == n){
+    //             cout << "*";
+    //         }
+    //         else{
+    //             cout << " ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= n; j++) {
+    //         if (i == 1 || i == n || j == 1 || j == n){
+    //             cout << "*";
+    //         }
+    //         else{
+    //             cout << "#";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= n; j++) {
+    //         if (i == 1 || i == n || j == 1 || j == n){
+    //             cout << i << " ";
+    //         }
+    //         else{
+    //             cout << "  ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+
+
+    //thuong giai thua
+
+    // int n; cin >> n;
+    // double res = 1;
+    // long long gt = 1;
+    // for (int i = 1; i <= n - 1; i++) {
+    //     gt *= i;
+    //     res += 1.0 / gt;
+    // }
+    // cout << fixed << setprecision(4) << res << endl;
+
+
+    //mua bia
+
+    // int n; cin >> n;
+    // int ans = n / 28;
+    // int vo = n / 28;
+    // while (vo >= 3) {
+    //     int temp = vo / 3;
+    //     ans += temp;
+    //     vo = vo % 3 + temp;
+    // }
+    // cout << ans << endl;
+
+
+    //digital root
+
+    // long long n;
+    // cin >> n;
+    // while (n >= 10) {
+    //     int sum = 0;
+    //     while (n > 0) {
+    //         sum += n % 10;
+    //         n /= 10;
+    //     }
+    //     n = sum;
+    // }
+    // cout << n << endl;
+
+
+    return 0;
+}
