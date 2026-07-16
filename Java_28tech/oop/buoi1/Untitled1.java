60429dec426551545e6c23dc0fc2f618c22292ad
chunglop0781 <chunglop0781@gmail.com>
2026-07-16 15:40:24 +0700

    Create Untitled1.java

---
+++ Java_28tech/oop/buoi1/Untitled1.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Untitled1 {
+    public static void main(String[] args){
+
+        // int a = 100, b = 200, c = 300;
+        // System.out.println("Gia tri cua 3 bien la: " + a + " " + b + " " + c);
+        // System.out.println("Gia tri cua 3 bien la :" + a + " " + b + " " + c); //Nối chuỗi
+        // System.out.printf("%.2f %.4f\n", d, e);
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // long b = sc.nextLong();
+        // System.out.println(a + " " + b);
+        // char c = sc.next().charAt(045);
+        // System.out.println(c);
+
+        // Scanner sc = new Scanner (System.in);
+        // int x = sc.nextInt();
+        // System.out.println(x);
+        // System.out.println("Hello World !");
+        // System.out.println("28tech C++ programming !");
+
+        // Scanner sc = new Scanner (System.in);
+        // int X = sc.nextInt();
+        // long Y = sc.nextLong();
+        // char C = sc.next().charAt(0);
+        // float F = sc.nextFloat();
+        // double D = sc.nextDouble();
+        // System.out.println(X);
+        // System.out.println(Y);
+        // System.out.println(C);
+        // System.out.printf("%.3f\n", F);
+        // System.out.printf("%.3f\n", D);
+
+        // int dai = 100, rong = 30;
+        // int chuVi = 2 * (dai + rong);
+        // System.out.println(chuVi);
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int chuVi = 2 * (a + b);
+        // double dienTich = (double) a * b;
+        // System.out.println("Chu vi HCN la: " + chuVi);
+        // System.out.printf("Dien tich HCN la: %.2f", dienTich);
+
+        // System.out.println(Math.abs(-500));
+        // System.out.println(Math.max(20, 15));
+        // System.out.println(Math.min(10, 3));
+        // System.out.println(Math.sqrt(30));
+        // System.out.println(Math.cbrt(28));
+        // System.out.println(Math.pow(2, 10));
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(), y = sc.nextInt(), z = sc.nextInt(), t = sc.nextInt(); 
+        System.out.println(y + "," + z + "," + x + "," + t); 
+        System.out.println((long)x + y + z + t);
+        System.out.println(x - y + (long)z * t);
+
+    }
+    
+}
