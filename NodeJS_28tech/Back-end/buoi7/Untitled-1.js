c67f7413a84b5210cc67a3a4788fbf926c2d3433
Hung Anh <hunganh.okokok.0@gmail.com>
2026-07-23 12:22:18 +0700

    Update 12-22-18_23-07-2026

---
+++ NodeJS_28tech/Back-end/buoi7/Untitled-1.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c);
+
+var hoVaTen = "Tao Ban Thien";
+console.log(hoVaTen);
+
+var fullName = "Le Van A";
+var email = "levana@gmail.com";
+var phone = "0123456789";
+
+var message = `Xin chào ${fullName}. Số điện thoại của bạn là ${phone}, email của bạn là ${email}`;
+console.log(message);
